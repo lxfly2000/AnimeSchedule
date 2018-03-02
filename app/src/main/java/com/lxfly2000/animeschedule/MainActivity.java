92b4d96576fb95cd1bd2b6f0fca64f203d5fad4e
lxfly2000 <gaoboyuan8@qq.com>
2018-03-02 22:25:47 +0800

    Update.

--- app/src/main/java/com/lxfly2000/animeschedule/MainActivity.java
+++ app/src/main/java/com/lxfly2000/animeschedule/MainActivity.java
@@ -1,14 +1,18 @@
 /*TODO:
-* 读取json文件并显示
+* 读取json文件并显示（OK）
+* 异步加载图片，更新列表显示
 * 根据json中项目的更新日期排序
-* 可增加/删除/修改项目，可直接在列表上标记观看集数，评分
-* 更新集数提示
+* 可增加/删除/长按修改项目，并保存至本地文件
+* 可直接在列表上标记观看集数
+* 点击打开链接（OK）
+* 更新集数提示（用对话框显示，可选择今日不再提示(Neu)/关闭(Posi)）
 * JGit上传/下载数据
 */
 
 package com.lxfly2000.animeschedule;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -16,15 +20,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.*;
 import com.lxfly2000.utilities.AndroidUtility;
 import com.lxfly2000.utilities.FileUtility;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     JSONObject animeJson;
@@ -48,17 +51,48 @@ public class MainActivity extends AppCompatActivity {
                 "android.permission.READ_EXTERNAL_STORAGE","No reading permission."))
             return;
         listAnime=(ListView)findViewById(R.id.listAnime);
+        listAnime.setOnItemClickListener(listAnimeCallback);
         try {
             animeJson=new JSONObject(FileUtility.ReadFile(Values.GetJsonDataFullPath()));
-            int count=animeJson.getJSONArray("anime").length();
-            ArrayList<String>titles=new ArrayList<>();
-            for(int i=0;i<count;i++){
-                titles.add(animeJson.getJSONArray("anime").getJSONObject(i).getString("title"));
-            }
-            listAnime.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,titles));
         }catch (JSONException e){
             Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
         }
+        DisplayList();
+    }
+
+    AdapterView.OnItemClickListener listAnimeCallback=new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(animeJson.getJSONArray("anime").getJSONObject(i).getString("watch_url"))));
+            }catch (JSONException e){
+                Toast.makeText(getBaseContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    public void DisplayList(){
+        ArrayList<HashMap<String,Object>>listItems=new ArrayList<>();
+        try{
+            for(int i=0;i<animeJson.getJSONArray("anime").length();i++){
+                HashMap<String,Object>listItem=new HashMap<>();
+                listItem.put("title",animeJson.getJSONArray("anime").getJSONObject(i).getString("title"));
+                listItem.put("description",animeJson.getJSONArray("anime").getJSONObject(i).getString("description"));
+                StringBuilder rankingString=new StringBuilder();
+                for(int j=0;j<5;j++){
+                    rankingString.append(j>animeJson.getJSONArray("anime").getJSONObject(i).getInt("rank")?"☆":"★");
+                }
+                listItem.put("ranking",rankingString.toString());
+                listItem.put("schedule",animeJson.getJSONArray("anime").getJSONObject(i).getString("start_date")+" 开始放送");
+                listItem.put("cover", Uri.parse(animeJson.getJSONArray("anime").getJSONObject(i).getString("cover")));
+                listItems.add(listItem);
+            }
+        }catch (JSONException e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+        String[]keyStrings={"title","description","ranking","schedule","cover"};
+        int[]viewIds={R.id.textAnimeTitle,R.id.textAnimeDescription,R.id.textRanking,R.id.textSchedule,R.id.imageCover};
+        listAnime.setAdapter(new SimpleAdapter(this,listItems,R.layout.item_anime,keyStrings,viewIds));
     }
 
     @Override

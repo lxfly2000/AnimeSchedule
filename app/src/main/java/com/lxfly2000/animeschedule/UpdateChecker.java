017765b404eeb92acc1201f13d217171948e1637
Yueyu <gaoboyuan8@qq.com>
2018-10-16 22:04:16 +0800

    Changed UpdateChecker executor.

--- app/src/main/java/com/lxfly2000/animeschedule/UpdateChecker.java
+++ app/src/main/java/com/lxfly2000/animeschedule/UpdateChecker.java
@@ -2,6 +2,7 @@ package com.lxfly2000.animeschedule;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import com.lxfly2000.utilities.AndroidDownloadFileTask;
 import com.lxfly2000.utilities.StreamUtility;
 
@@ -38,7 +39,7 @@ public class UpdateChecker {
             }
         };
         task.SetExtra(this);
-        task.execute(fileURL);
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,fileURL);
     }
 
     public boolean IsError(){

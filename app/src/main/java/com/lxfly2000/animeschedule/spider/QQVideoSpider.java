84ca1d97bc1b54353559b039e85c616e5cabc551
lxfly2000 <gaoboyuan8@qq.com>
2019-05-02 10:34:51 +0800

    Add Youku & QQ Video spiders.

--- app/src/main/java/com/lxfly2000/animeschedule/spider/QQVideoSpider.java
+++ app/src/main/java/com/lxfly2000/animeschedule/spider/QQVideoSpider.java
@@ -9,7 +9,13 @@ public class QQVideoSpider extends Spider {
 
     @Override
     public void Execute(String url){
-        onReturnDataFunction.OnReturnData(null,STATUS_FAILED,"非常抱歉，针对腾讯视频的信息获取功能还在制作中，请手动填写番剧信息。");
+        //腾讯网址格式：
+        //应用&移动端：m.v.qq.com/play/play.html?coverid=<CoverID>&vid=<VID>&vuid=<VUID>&(后面的参数可以不用管)
+        //                                       ~~~~~~~也称为cid，其实只管这个CoverID就可以了
+        //网页端：①v.qq.com/x/cover/<CoverID>.html
+        //　　　　②v.qq.com/x/cover/<CoverID>/<EpID>.html
+        //                                     ~~~~~~同样也只需要管CoverID就行了
+        onReturnDataFunction.OnReturnData(null,STATUS_FAILED,"非常抱歉，针对腾讯视频的信息获取功能还在制作中，请手动填写番剧信息。\n如果您是开发者，欢迎到Github帮我完善这个功能。");
         //TODO：对给出的播放页URL获取信息
     }
 }
--- app/src/main/java/com/lxfly2000/animeschedule/spider/YoukuSpider.java
+++ app/src/main/java/com/lxfly2000/animeschedule/spider/YoukuSpider.java
@@ -9,7 +9,11 @@ public class YoukuSpider extends Spider {
 
     @Override
     public void Execute(String url){
-        onReturnDataFunction.OnReturnData(null,STATUS_FAILED,"非常抱歉，针对优酷的信息获取功能还在制作中，请手动填写番剧信息。");
+        //优酷网址格式：
+        //应用&移动端：m.youku.com/video/id_<VID>.html
+        //网页端视频播放页：v.youku.com/v_show/id_<VID>.html
+        //网页端剧集简介：list.youku.com/show/id_<ListID>.html
+        onReturnDataFunction.OnReturnData(null,STATUS_FAILED,"非常抱歉，针对优酷的信息获取功能还在制作中，请手动填写番剧信息。\n如果您是开发者，欢迎到Github帮我完善这个功能。");
         //TODO：对给出的播放页URL获取信息
     }
 }

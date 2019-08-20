d103801c6e66b51522bddb2ed93f78de5f12f921
lxfly2000 <gaoboyuan8@qq.com>
2019-08-20 14:00:42 +0800

    Ignore lint errors produced by external libraries.

--- app/src/test/java/com/lxfly2000/animeschedule/TestFlvMerge.java
+++ app/src/test/java/com/lxfly2000/animeschedule/TestFlvMerge.java
@@ -1,5 +1,6 @@
 package com.lxfly2000.animeschedule;
 
+import com.lxfly2000.utilities.FileUtility;
 import com.lxfly2000.youget.joiner.FLVJoiner;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,6 +12,10 @@ public class TestFlvMerge {
                 "D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\1.blv",
                 "D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\2.blv",
                 "D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\3.blv"};
+        for (String input : inputs) {
+            if (!FileUtility.IsFileExists(input))
+                return;
+        }
         Assert.assertEquals(0,new FLVJoiner().join(inputs,"TestOutput.flv"));
     }
 }
--- app/src/test/java/com/lxfly2000/animeschedule/TestMp4Parser.java
+++ app/src/test/java/com/lxfly2000/animeschedule/TestMp4Parser.java
@@ -1,5 +1,6 @@
 package com.lxfly2000.animeschedule;
 
+import com.lxfly2000.utilities.FileUtility;
 import com.lxfly2000.youget.joiner.MP4Joiner;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,10 @@ public class TestMp4Parser {
     @Test
     public void TestMerge(){
         String[]inputs={"D:\\Yueyu\\Anime\\咕噜咕噜魔法阵剧场\\12_ffmpeg.mp4","D:\\Yueyu\\Anime\\咕噜咕噜魔法阵剧场\\13_ffmpeg.mp4"};
+        for (String input : inputs) {
+            if (!FileUtility.IsFileExists(input))
+                return;
+        }
         Assert.assertEquals(0,new MP4Joiner().join(inputs,"TestOutput.mp4"));
     }
 }

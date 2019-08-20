71c65748722c3a6417e3bd6fee9bf7465ebb1c5d
lxfly2000 <gaoboyuan8@qq.com>
2019-08-21 07:34:58 +0800

    Update dn dlg.

--- app/src/main/java/com/lxfly2000/animeschedule/downloaddialog/IQiyiDownloadDialog.java
+++ app/src/main/java/com/lxfly2000/animeschedule/downloaddialog/IQiyiDownloadDialog.java
@@ -4,16 +4,42 @@
 package com.lxfly2000.animeschedule.downloaddialog;
 
 import android.content.Context;
-import android.widget.TextView;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import com.lxfly2000.animeschedule.AnimeJson;
 import com.lxfly2000.animeschedule.R;
+import com.lxfly2000.animeschedule.Values;
+import com.lxfly2000.animeschedule.data.AnimeItem;
+import com.lxfly2000.animeschedule.spider.IQiyiSpider;
+import com.lxfly2000.animeschedule.spider.Spider;
+
+import java.util.ArrayList;
 
 public class IQiyiDownloadDialog extends DownloadDialog {
+    private SharedPreferences preferences;
     public IQiyiDownloadDialog(@NonNull Context context) {
         super(context);
+        checkEpisodes=new ArrayList<>();
+        preferences= Values.GetPreference(context);
     }
+
+    ArrayList<CheckBox>checkEpisodes;
+    Button buttonOk;
+    LinearLayout linearLayout;
+
+    private CompoundButton.OnCheckedChangeListener checkedChangeListener=(compoundButton, b) -> {
+        int checkedCount=0;
+        for(CheckBox checkBox:checkEpisodes){
+            if(checkBox.isChecked())
+                checkedCount++;
+        }
+        buttonOk.setEnabled(checkedCount>0);
+    };
+
+    private boolean episodeTitleOK=false;
 
     @Override
     public void OpenDownloadDialog(AnimeJson json, int index) {
@@ -23,11 +49,38 @@ public class IQiyiDownloadDialog extends DownloadDialog {
                     AlertDialog dq=new AlertDialog.Builder(ctx)
                             .setTitle(json.GetTitle(index))
                             .setView(R.layout.dialog_anime_download_choose_quality)
-                            .setPositiveButton(android.R.string.ok,null)
+                            .setPositiveButton("爱奇艺的下载功能正在制作中，敬请期待！",null)
                             .show();
                 })
+                .setNegativeButton(android.R.string.cancel,null)
                 .setView(R.layout.dialog_anime_download_with_notice)
                 .show();
+        linearLayout=dialog.findViewById(R.id.linearLayoutEpisodes);
+        buttonOk=dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        buttonOk.setEnabled(false);
+        IQiyiSpider spider=new IQiyiSpider(ctx);
+        spider.SetOnReturnDataFunction(new Spider.OnReturnDataFunction() {
+            @Override
+            public void OnReturnData(AnimeItem data, int status, String resultMessage, int focusId) {
+                if(resultMessage!=null)
+                    Toast.makeText(ctx,resultMessage,Toast.LENGTH_LONG).show();
+                if(status==Spider.STATUS_FAILED)
+                    return;
+                if(data.title!=null)
+                    dialog.setTitle(data.title);
+                if(episodeTitleOK)
+                    return;
+                for(int i=0;i<data.episodeTitles.size();i++){
+                    CheckBox checkBox=new CheckBox(dialog.getContext());
+                    checkBox.setText("["+data.episodeTitles.get(i).episodeIndex+"] "+data.episodeTitles.get(i).episodeTitle);
+                    checkBox.setOnCheckedChangeListener(checkedChangeListener);
+                    checkEpisodes.add(checkBox);
+                    linearLayout.addView(checkBox);
+                    episodeTitleOK=true;
+                }
+            }
+        });
+        spider.Execute(json.GetWatchUrl(index));
         ((TextView)dialog.findViewById(R.id.textViewDownloadNotice)).setText(R.string.label_iqiyi_download_notice);
     }
 }

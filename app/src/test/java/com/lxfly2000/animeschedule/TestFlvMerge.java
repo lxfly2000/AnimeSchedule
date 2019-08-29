dd90eae7ba4eb97f453d284ff55592f4b2d1c366
lxfly2000 <gaoboyuan8@qq.com>
2019-08-29 22:04:32 +0800

    Revert "Update."
    This reverts commit 99502775

--- app/src/test/java/com/lxfly2000/animeschedule/TestFlvMerge.java
+++ app/src/test/java/com/lxfly2000/animeschedule/TestFlvMerge.java
@@ -8,15 +8,15 @@ import org.junit.Test;
 public class TestFlvMerge {
     @Test
     public void Merge(){
-        String[]inputs={"E:\\gaoboyuan\\lua.flv.bb2api.80\\0.blv",
-                "E:\\gaoboyuan\\lua.flv.bb2api.80\\1.blv",
-                "E:\\gaoboyuan\\lua.flv.bb2api.80\\2.blv",
-                "E:\\gaoboyuan\\lua.flv.bb2api.80\\3.blv",
-                "E:\\gaoboyuan\\lua.flv.bb2api.80\\4.blv"};
+        String[]inputs={"D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\0.blv",
+                "D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\1.blv",
+                "D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\2.blv",
+                "D:\\Yueyu\\Bilibili\\download\\s_5800\\98609\\lua.flv.bb2api.80\\3.blv"};
         for (String input : inputs) {
             if (!FileUtility.IsFileExists(input))
                 return;
         }
-        Assert.assertEquals(0,new FLVJoiner().join(inputs,"E:\\gaoboyuan\\lua.flv.bb2api.80\\merged.flv"));
+        Assert.assertEquals(0,new FLVJoiner().join(inputs,"TestOutput.flv"));
+        FileUtility.DeleteFile("TestOutput.flv");
     }
 }
